48a5f7855372487fe990a92a914e8e930a3da66c
sara karam <sarakaram966@gmail.com>
2025-05-10 18:54:38 +0300

    edit stockvaluecaluation

--- StockValueCalculation.java
+++ StockValueCalculation.java
@@ -1,14 +1,4 @@
-// public class StockValueCalculation implements ValueCalculationStrategy {
-//     private final Stock stock;
-//     public StockValueCalculation(Stock stock) {
-//         this.stock = stock;
-//     } 
-//     @Override
-//     public double calculateValue() {
-//         return stock.getQuantity() * stock.getPurchasePrice();
 
-//     }
-// }
 public class StockValueCalculation implements ValueCalculationStrategy {
     @Override
     public double calculateValue(Asset asset) {
